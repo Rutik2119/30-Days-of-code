4f4ea46f0620a08bcefc0b40489806f6348e279a
Rutik Suresh Narwade <68146965+Rutik2119@users.noreply.github.com>
2020-08-24 19:07:25 +0530

    Add files via upload

---
+++ Day 16 Exceptions  String to Integer.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import sys
+try:
+    print(int(input()))
+except ValueError:
+    print("Bad String")
+
+
+S = input().strip()
+try:
+    print(int(input()))
+except ValueError:
+    print("Bad String")
